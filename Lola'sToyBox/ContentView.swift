900f229c946871abad89516a46eb1f6b66bc7dcb
Russell Gordon <github@russellgordon.ca>
2022-03-11 06:00:17 -0500

    Reformatted code using Command-A, Control-I sequence.

--- Lola'sToyBox/ContentView.swift
+++ Lola'sToyBox/ContentView.swift
@@ -7,76 +7,76 @@
 import SwiftUI
 
 struct ContentView: View {
-var body: some View {
-//NavigationView {
-ZStack {
- Image("WoodBack")
- .resizable()
- .edgesIgnoringSafeArea(.all)
-    
-VStack {
-    Button {
-        print("Edit button was tapped")
-    } label: {
-        Image("PawPrint")
-            .resizable()
-            .frame(width: 100, height: 100)
-        .clipShape(Capsule())
-        .padding()
-    }
-        Button {
-            print("Edit button was tapped")
-        } label: {
-            Image("PawPrint")
+    var body: some View {
+        //NavigationView {
+        ZStack {
+            Image("WoodBack")
                 .resizable()
-                .frame(width: 100, height: 100)
-            .clipShape(Capsule())
-            .padding()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image("PawPrint")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image("PawPrint")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image("PawPrint")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image("PawPrint")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image("PawPrint")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Capsule())
+                        .padding()
+                    
+                }
+            }
+            
+            
+            .statusBar(hidden: true)
+            .preferredColorScheme(.light)
+            
+            //}
+            
+            
         }
-    Button {
-        print("Edit button was tapped")
-    } label: {
-        Image("PawPrint")
-            .resizable()
-            .frame(width: 100, height: 100)
-        .clipShape(Capsule())
-        .padding()
     }
-    Button {
-        print("Edit button was tapped")
-    } label: {
-        Image("PawPrint")
-            .resizable()
-            .frame(width: 100, height: 100)
-        .clipShape(Capsule())
-        .padding()
-    }
-    Button {
-        print("Edit button was tapped")
-    } label: {
-        Image("PawPrint")
-            .resizable()
-            .frame(width: 100, height: 100)
-        .clipShape(Capsule())
-        .padding()
-        
-    }
-    }
-
-
-.statusBar(hidden: true)
-.preferredColorScheme(.light)
-
-//}
-
-
- }
-}
 }
 
 struct ContentView_Previews: PreviewProvider {
-static var previews: some View {
-ContentView()
- }
+    static var previews: some View {
+        ContentView()
+    }
 }
 
