7fbbe7e6e129824d0a6abd4ba8a7191503ffb809
Russell Gordon <github@russellgordon.ca>
2022-03-11 06:26:58 -0500

    Added properties to track which video is selected.

--- Lola'sToyBox/Views/ContentView.swift
+++ Lola'sToyBox/Views/ContentView.swift
@@ -7,6 +7,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+
+    // List of mixed remote and local/bundled videos
+    let availableVideos: [Video] = [
+        
+        Video(url: Bundle.main.url(forResource: "videoCAT", withExtension: "MP4")!),
+        Video(url: Bundle.main.url(forResource: "VideoCAT-Rotated-To-Vertical", withExtension: "mov")!),
+        Video(url: Bundle.main.url(forResource: "VideoCAT-Rotated-Back-To-Horizontal", withExtension: "mov")!),
+        Video(url: Bundle.main.url(forResource: "Jake-Example-Video-4", withExtension: "mp4")!),
+        Video(url: Bundle.main.url(forResource: "Jake-Example-Video-5", withExtension: "mp4")!),
+
+    ]
+
+    // Which of these videos is selected for display in the full screen overlay
+    @State private var selectedVideo: Video?
+    
+    // MARK: Computed properties
     var body: some View {
         //NavigationView {
         ZStack {
@@ -16,7 +34,10 @@ struct ContentView: View {
             
             VStack {
                 Button {
-                    print("Edit button was tapped")
+                    print("Edit button 1 was tapped")
+                    
+                    // Select the first video
+                    selectedVideo = availableVideos[0]
                 } label: {
                     Image("PawPrint")
                         .resizable()
@@ -24,8 +45,12 @@ struct ContentView: View {
                         .clipShape(Capsule())
                         .padding()
                 }
+
                 Button {
-                    print("Edit button was tapped")
+                    print("Edit button 2 was tapped")
+
+                    // Select the second video
+                    selectedVideo = availableVideos[2]
                 } label: {
                     Image("PawPrint")
                         .resizable()
@@ -33,8 +58,12 @@ struct ContentView: View {
                         .clipShape(Capsule())
                         .padding()
                 }
+                
                 Button {
-                    print("Edit button was tapped")
+                    print("Edit button 3 was tapped")
+
+                    // Select the third video
+                    selectedVideo = availableVideos[2]
                 } label: {
                     Image("PawPrint")
                         .resizable()
@@ -42,8 +71,12 @@ struct ContentView: View {
                         .clipShape(Capsule())
                         .padding()
                 }
+
                 Button {
-                    print("Edit button was tapped")
+                    print("Edit button 4 was tapped")
+
+                    // Select the fourth video
+                    selectedVideo = availableVideos[3]
                 } label: {
                     Image("PawPrint")
                         .resizable()
@@ -51,8 +84,12 @@ struct ContentView: View {
                         .clipShape(Capsule())
                         .padding()
                 }
+
                 Button {
-                    print("Edit button was tapped")
+                    print("Edit button 4 was tapped")
+
+                    // Select the fifth video
+                    selectedVideo = availableVideos[4]
                 } label: {
                     Image("PawPrint")
                         .resizable()
@@ -61,9 +98,8 @@ struct ContentView: View {
                         .padding()
                     
                 }
+                
             }
-            
-            
             .statusBar(hidden: true)
             .preferredColorScheme(.light)
             
